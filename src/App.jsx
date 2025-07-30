bf7f070dd77861c0d4a28a05ee1450d2445da5d5
Harshitha2300033363 <2300033363@kluniversity.in>
2025-07-30 13:58:40 +0530

    Centered content and added logos

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,23 @@
+import React from 'react'
+import viteLogo from '/vite.svg'
+import reactLogo from './assets/react.svg'
+import './App.css'
+
 function App() {
+  const [count, setCount] = React.useState(0)
+
   return (
-    <>
+    <div className="container">
+      <div className="logos">
+        <img src={viteLogo} className="logo" alt="Vite logo" />
+        <img src={reactLogo} className="logo react" alt="React logo" />
+      </div>
       <h1>Vite + React</h1>
-      <h3>Hi, this is SKILL 3 - Applying CI/CD for react project using git actions</h3>
-      <p>count is 0</p>
-    </>
-  );
+      <p>Hi, this is SKILL 3 - Applying CI/CD for react project using git actions</p>
+      <p>count is {count}</p>
+      <button onClick={() => setCount(count + 1)}>Increase Count</button>
+    </div>
+  )
 }
 
-export default App;
+export default App
