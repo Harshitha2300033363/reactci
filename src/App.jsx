c56ff731b5f8b48b9c3a3b6e61a104b7afded511
Harshitha2300033363 <2300033363@kluniversity.in>
2025-07-30 11:37:36 +0530

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <>
+      <h1>Vite + React</h1>
+      <h3>Hi, this is SKILL 3 - Applying CI/CD for react project using git actions</h3>
+      <p>count is 0</p>
+    </>
+  );
+}
+
+export default App;
